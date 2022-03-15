7cf7ea006c6fda1345a15a1475f1a5b481cb4392
pjprateek0505 <101003901+pjprateek0505@users.noreply.github.com>
2022-03-15 09:59:51 +0530

    Add files via upload

---
+++ milestone 4(recursion)/recursion 3.cpp
@@ -0,0 +1,40 @@
+// recursion 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int fib(int x)
+{
+    if ((x == 1) || (x == 0))
+    {
+        return x;
+    }
+    else
+    {
+        return (fib(x - 1) + fib(x - 2));
+    }
+}
+int main()
+{
+    int x, i = 0;
+    cout << "enter the number of terms in series:";
+    cin >> x;
+    cout << "fibonacci series:";
+    while (i < x)
+    {
+        cout << " " << fib(i);
+        i++;
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
